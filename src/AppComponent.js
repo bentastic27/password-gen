6c486542b8c54aa49e5d7cc04b4f2e132667c264
Ben Healey <bentastic27@gmail.com>
2017-12-21 01:34:53 -0500

    Got rid of react-toolbox for now. Will likely continue with just bootstrap to avoid styling issues.

--- src/AppComponent.js
+++ src/AppComponent.js
@@ -7,6 +7,9 @@ class AppComponent extends React.Component {
       <Layout>
         <h1>Password Generator</h1>
         <pre>{this.props.password}</pre>
+        <button onClick={this.props.generatePassword}>
+          Generate Password
+        </button>
       </Layout>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
-import ThemeProvider from 'react-toolbox/lib/ThemeProvider';
-import theme from './toolbox/theme';
 import AppContainer from './AppContainer';
 
-const app = (
-  <ThemeProvider theme={theme}>
-    <AppContainer />
-  </ThemeProvider>
-);
-
-ReactDOM.render(app, document.getElementById('root'));
+ReactDOM.render(<AppContainer/>, document.getElementById('root'));
 registerServiceWorker();
